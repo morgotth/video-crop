89b511eaf2aeaf1f9d095a24e9b57eb3f2dd2a6f
Morgotth <morgotth0@gmail.com>
2015-02-14 22:46:42 +0100

    Update Copyright

--- README.md
+++ README.md
@@ -110,6 +110,7 @@ TextureVideoView.setListener(MediaPlayerListener)
 The MIT License (MIT)
 
 Copyright (c) 2014 Danylyk Dmytro
+Copyright (c) 2014 Jacob Tabak
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- library/src/main/java/com/dd/crop/TextureVideoView.java
+++ library/src/main/java/com/dd/crop/TextureVideoView.java
@@ -17,6 +17,7 @@ import java.io.IOException;
  *    The MIT License (MIT)
  *
  *   Copyright (c) 2014 Danylyk Dmytro
+ *   Copyright (c) 2014 Jacob Tabak
  *
  *   Permission is hereby granted, free of charge, to any person obtaining a copy
  *   of this software and associated documentation files (the "Software"), to deal
